499aaee8911f97ab946d5c939a0f057798a4e0d6
Tareq Abu Alhayjaa <128913771+taraq-abualhija@users.noreply.github.com>
2023-10-20 15:05:02 +0300

    first commit

---
+++ Hosting/javascript/sidebars.js
@@ -0,0 +1,14 @@
+/* global bootstrap: false */
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
+
+
+
+
+
+
